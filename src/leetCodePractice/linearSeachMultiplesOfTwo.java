a99a3cdd0ade3023761449b4dacb192ba9580cf5
eric_xiao <xiao.yuan0217@gmail.com>
2021-03-18 16:27:22 -0400

    Updated linear search multiples of two. Used to check if there exist an element that is doubled another element in an array.

--- src/leetCodePractice/linearSeachMultiplesOfTwo.java
+++ src/leetCodePractice/linearSeachMultiplesOfTwo.java
@@ -10,10 +10,8 @@ public class linearSeachMultiplesOfTwo {
         for (int i = 0; i < arr.length; i++) {
         	for (int j = i+1; j < arr.length; j++ ) {
         		if (arr[i] == arr[j] * 2) {
-        			System.out.print(i + " " + j);
         			return true;
         		} else if (arr[i] * 2 == arr[j]) {
-        			System.out.print(i + " " + j);
         			return true;
         		}
         	}
